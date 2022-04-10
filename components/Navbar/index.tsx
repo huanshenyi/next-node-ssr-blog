88191b3051dad30361981301d605a837cf11e978
huanshenyi <txy1226052@gmail.com>
2022-04-10 09:59:06 +0900

    navデザインの修正

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,16 +1,20 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import styles from './index.module.scss';
 import { navs } from 'components/Navbar/config';
 
 const Navbar: NextPage = () => {
+  const { pathname } = useRouter();
   return (
     <div className={styles.navbar}>
       <section className={styles.logoArea}>BLOG-C</section>
       <section className={styles.linkArea}>
         {navs?.map((nav) => (
           <Link key={nav?.label} href={nav?.value}>
-            <a>{nav?.label}</a>
+            <a className={pathname === nav.value ? styles.active : ''}>
+              {nav?.label}
+            </a>
           </Link>
         ))}
       </section>
