1b49ef26334189112d9ea00343971cebae8c56a4
huanshenyi <txy1226052@gmail.com>
2022-04-21 09:17:10 +0900

    login modalの基本構造

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,12 +1,63 @@
+import { useState } from 'react';
 import { NextPage } from 'next';
+import styles from './index.module.scss';
 
 interface Iprops {
   isShow: boolean;
   onClose: () => void;
 }
 
-const Login: NextPage<Iprops> = (props) => {
-  return <>login</>;
+const Login: NextPage<Iprops> = ({ isShow = false, onClose }) => {
+  const [form, setForm] = useState({
+    phone: '',
+    verify: '',
+  });
+  const handleClose = () => {};
+  const handelGetVerifyCode = () => {};
+  const handleLogin = () => {};
+  const handleOAuthGithub = () => {};
+
+  return isShow ? (
+    <div className={styles.loginArea}>
+      <div className={styles.loginBox}>
+        <div className={styles.loginTitle}>
+          <div>phone登録</div>
+          <div className={styles.close} onClick={handleClose}>
+            x
+          </div>
+        </div>
+        <input
+          type="text"
+          name="phone"
+          placeholder="携帯番号入力"
+          value={form.phone}
+        />
+        <div className={styles.verifyCodeArea}>
+          <input
+            type="text"
+            name="verify"
+            placeholder="確認コード入力"
+            value={form.verify}
+          />
+          <span className={styles.verifyCode} onClick={handelGetVerifyCode}>
+            コード取得
+          </span>
+        </div>
+        <div className={styles.loginBtn} onClick={handleLogin}>
+          ログイン
+        </div>
+        <div className={styles.otherLogin} onClick={handleOAuthGithub}>
+          githubログイン
+        </div>
+        <div className={styles.loginPrivacy}>
+          ログインイコール同意
+          <a href="" target="_blank" rel="noreferrer">
+            プライバシー何か
+          </a>
+        </div>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default Login;
